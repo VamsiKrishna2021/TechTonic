5cf941bfe80001c51346cc67825d645c6a4ccefd
Vamsi Krishna Madhabattula <vamsimadhabattula@gmail.com>
2024-11-01 18:04:41 +0000

    Apped Product route

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 
-
 app.use(express.json());  // allows us to accept JSON data in the body
 
-app.post("/api/products", async (req,res) => {
-    const product = req.body; //User will send this data
-
-    if(!product.name || !product.price || !product.image) {
-        return res.statusCode(400).json({ success:false, message: "Please provide all fields"});
-    }
-
-    const newProduct = new Product(product);
-    
-    try{
-        await newProduct.save();
-        res.status(201).json({success: true, data: newProduct});
-    } catch (error) {
-        console.error("Error in creating product:",error.message);
-        res.status(500).json({ success: false, message: "Server Error"});
-    }    
-});
-
-// postman desktop app
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
     connectDB();
