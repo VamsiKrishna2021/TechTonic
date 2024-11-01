06581d89f3338649d268231f46559782a8d6f93d
Vamsi Krishna Madhabattula <vamsimadhabattula@gmail.com>
2024-11-01 01:45:58 +0000

    Redo Changes

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/products", (req,res) => {
+    
+})
+
+app.listen(5000, () => {
+    console.log("Server Started at http://localhost:5000 ");
+})
+
+
+// EmTdmshVRo0YmJ4g
