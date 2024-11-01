3f6b6bf15782db7bfa787e8536907badb9667b93
Vamsi Krishna Madhabattula <vamsimadhabattula@gmail.com>
2024-11-01 03:19:20 +0000

    Added Schema

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,37 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/products", (req,res) => {
+
+app.use(express.json());  // allows us to accept JSON data in the body
+
+app.post("/api/products", async (req,res) => {
+    const product = req.body; //User will send this data
+
+    if(!product.name || !product.price || !product.image) {
+        return res.statusCode(400).json({ success:false, message: "Please provide all fields"});
+    }
+
+    const newProduct = new Product(product)
     
-})
+    try{
+        await newProduct.save();
+        res.status(201).json({success: true, data: newProduct});
+    } catch (error) {
+        console.error("Error in creating product:",error.message);
+        res.status(500).json({ success: false, message: "Server Error"});
+    }    
+});
+
+// postman desktop app
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server Started at http://localhost:5000 ");
 })
 
