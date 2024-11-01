eb89d79238069c947f7a4e9294f529c5299fec1f
VamsiKrishna2021 <81315845+VamsiKrishna2021@users.noreply.github.com>
2024-11-01 17:41:18 -0500

    Made changes in Front end SRC

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import { Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import Createpage from "./pages/CreatePage";
+import Homepage from "./pages/Homepage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <Button>Hello</Button>
-    </>
+    <Box minH={"100vh"}>
+      {<Navbar/>}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/create" element={<Createpage />} />
+      </Routes>
+
+    </Box>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <ChakraProvider >
-          <App />
-      </ChakraProvider >
+     <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>          
   </React.StrictMode>,
 );
