7690703acd2c089d4da034442414aa0f72ab9465
Vamsi Krishna Madhabattula <vamsimadhabattula@gmail.com>
2024-11-01 18:44:02 +0000

    Completed backend part

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());  // allows us to accept JSON data in the body
 
@@ -14,5 +14,5 @@ app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
     connectDB();
-    console.log("Server Started at http://localhost:5000 ");
+    console.log("Server Started at http://localhost:" +PORT);
 })
